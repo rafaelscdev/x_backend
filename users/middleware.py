f8ad5d8ca5a8cad7eda7f1e0b29b1707eeaa5917
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-18 16:57:37 -0300

    style: aplica Black e isort em todo o projeto

--- users/middleware.py
+++ users/middleware.py
@@ -3,5 +3,5 @@ from django.utils.deprecation import MiddlewareMixin
 
 class DisableCSRFOnAPI(MiddlewareMixin):
     def process_request(self, request):
-        if request.path.startswith('/api/'):
-            setattr(request, '_dont_enforce_csrf_checks', True) 
+        if request.path.startswith("/api/"):
+            setattr(request, "_dont_enforce_csrf_checks", True)
