55b9c0c82e92c10f5550a23b4ea59e4762b85dd1
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-26 16:36:36 -0300

    feat: adiciona endpoint JWT /api/token/ para autenticação

--- x_backend_api/urls.py
+++ x_backend_api/urls.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
