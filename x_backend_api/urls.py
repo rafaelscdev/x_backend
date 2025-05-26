9ad15728cab8f2e683fcbd63add0cef451f25a87
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-26 16:42:29 -0300

    style: formata urls.py com black

--- x_backend_api/urls.py
+++ x_backend_api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("api/users/", include("users.urls")),
     path("api/posts/", include("posts.urls")),
     path("api/follows/", include("follows.urls")),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
